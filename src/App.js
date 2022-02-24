76304b85d7ed08f686c4a9848d9102fe20957e7f
Jansen O <ophelie.jansen@applidget.com>
2022-02-24 15:58:57 +0100

    add expenses constant + call ExpenseItem multiple times w/ props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import ExpenseItem from './components/ExpenseItem';
+import { expenses } from './expenses'
 
 function App() {
+  const expenseItems = expenses.map(expense => {
+    return <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      date={expense.date}
+      amount={expense.amount} />
+  });
+
   return (
     <div>
       <h2>Let's get started!</h2>
-
-      <ExpenseItem></ExpenseItem>
+      {expenseItems}
     </div>
   );
 }
