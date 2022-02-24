b1d903d2cf2226b906cd6c9615d89e9bc7d8eb10
Jansen O <ophelie.jansen@applidget.com>
2022-02-24 16:09:43 +0100

    add ExpenseDate style

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,15 @@
+import './ExpenseDate.css'
+
 function ExpenseDate(props) {
   const { date } = props;
   const year = date.toLocaleString('en-US', { year: 'numeric' });
   const month = date.toLocaleString('en-US', { month: 'long' });
   const day = date.toLocaleString('en-US', { day: '2-digit' });
 
-  return (<div>
-    <div>{month}</div>
-    <div>{year}</div>
-    <div>{day}</div>
+  return (<div className='expense-date'>
+    <div className='expense-date__month'>{month}</div>
+    <div className='expense-date__year'>{year}</div>
+    <div className='expense-date__day'>{day}</div>
   </div>);
 }
 
