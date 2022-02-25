a059ff73a6177d67514f7f7082bd17d8e78279fc
Jansen O <ophelie.jansen@applidget.com>
2022-02-25 15:54:03 +0100

    add filter on expenses list

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,19 +1,26 @@
+import { useState } from 'react';
+
 import './Expenses.css';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from '../filters/ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
   const { items } = props;
-  const expenses = items.map(expense => {
-    return <ExpenseItem
-    key={expense.id}
-    title={expense.title}
-    date={expense.date}
-    amount={expense.amount} />
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
   });
 
   return (<Card className='expenses'>
-    {expenses}
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+    <ExpensesList items={filteredExpenses} />
   </Card>);
 }
 
